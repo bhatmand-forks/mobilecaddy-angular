9232d313b8d863922b8f1d0338778062ea2c561b
Todd Halfpenny <todd@mobilecaddy.net>
2018-05-21 20:47:17 +0100

    Add outbox-page (for inside MobileCaddy settings)

---
+++ mobilecaddy-angular/src/outbox-page/outbox-page.ts
@@ -0,0 +1,12 @@
+import { Component, Inject } from '@angular/core';
+
+// import { MobileCaddySyncService } from '../mobilecaddy-sync-service/mobilecaddy-sync-service.service';
+// import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-outbox',
+  templateUrl: 'outbox-page.html'
+})
+export class MCOutboxPage {
+  constructor() {}
+}
